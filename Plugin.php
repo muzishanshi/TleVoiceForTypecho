3e7d8d5a2665648f396a230a1d96269f8e8ecf90
muzishanshi <muzishanshi@126.com>
2018-07-13 14:12:21 +0800

    Diamond

--- Plugin.php
+++ Plugin.php
@@ -3,9 +3,9 @@
  * TleVoice同乐语音，使用了百度语音api实现，可帮助网站个性化文章阅读
  * @package TleVoice For Typecho
  * @author 二呆
- * @version 1.0.1
+ * @version 1.0.2
  * @link http://www.tongleer.com/
- * @date 2018-07-04
+ * @date 2018-07-13
  */
 
 class TleVoice_Plugin implements Typecho_Plugin_Interface{
@@ -22,7 +22,7 @@ class TleVoice_Plugin implements Typecho_Plugin_Interface{
     // 插件配置面板
     public static function config(Typecho_Widget_Helper_Form $form){
 		//版本检查
-		$version=file_get_contents('http://api.tongleer.com/interface/TleVoice.php?action=update&version=1');
+		$version=file_get_contents('http://api.tongleer.com/interface/TleVoice.php?action=update&version=2');
 		$div=new Typecho_Widget_Helper_Layout();
 		$div->html('版本检查：'.$version);
 		$div->render();
@@ -35,9 +35,9 @@ class TleVoice_Plugin implements Typecho_Plugin_Interface{
 		$secret_key = @isset($_POST['secret_key']) ? addslashes(trim($_POST['secret_key'])) : '';
 		if($app_id!=''&&$api_key!=''&&$secret_key!=''){
 			file_put_contents(dirname(__FILE__).'/config.php','<?php die; ?>'.serialize(array(
-				'app_id'=>$app_id,
-				'api_key'=>$api_key,
-				'secret_key'=>$secret_key
+				'app_id'=>$app_id,//10519990
+				'api_key'=>$api_key,//1DsFzRqqj4jtWXBYNLfGb0GWVeXthUv5
+				'secret_key'=>$secret_key//dhDCcPyDCa6vgi5028WKT4x4SrUzD8YY
 			)));
 		}
 		
@@ -49,7 +49,7 @@ class TleVoice_Plugin implements Typecho_Plugin_Interface{
 			<h6>使用方法</h6>
 			<span>
 				第一步：写文章时以下面这种格式添加一个名为gif的字段；
-				<pre><font color="blue">第一人gif图片地址`{number}`第一人英文名`第一人中文名`第一人说话内容|第二人gif图片地址`{number}`第二人英文名`第二人中文名`第二人说话内容</font>……以此类推<br />1、每个对话之间用“<font color="red">|</font>”分割，对话中每个项目用“<font color="red">`</font>”分割。<br />2、{number}位置填写0、1、3、4，0代表普通女声、1代表普通男声、3代表情感男声、4代表情感女声。<br />3、注意同一文章中每个英文名不能相同。
+				<pre><font color="blue">第一人gif图片地址`{number}`第一人英文名/序列(不可重复)`第一人中文名`第一人说话内容|第二人gif图片地址`{number}`第二人英文名/序列(不可重复)`第二人中文名`第二人说话内容</font>……以此类推<br />1、每个对话之间用“<font color="red">|</font>”分割，对话中每个项目用“<font color="red">`</font>”分割。<br />2、{number}位置填写0、1、3、4，0代表普通女声、1代表普通男声、3代表情感男声、4代表情感女声。<br />3、注意同一文章中每个英文名不能相同。
 				</pre>
 			</span>
 			<span><p>第二步：在本页文章列表中点击生成语音；</p></span>
@@ -186,7 +186,7 @@ class TleVoice_Plugin implements Typecho_Plugin_Interface{
 						$out.='
 							<div class="am-modal am-modal-alert my-alert" tabindex="-1" id="my-alert'.$index.'">
 							  <div class="am-modal-dialog">
-								<div class="am-modal-hd"><img src="'.$values[0].'" /></div>
+								<div class="am-modal-hd"><img src="'.$values[0].'" width="200" /></div>
 								<div class="am-modal-bd" style="height:150px;">
 								  '.$values[3].'说：'.$values[4].'<br />
 								  <audio src="'.$plug_url.'/TleVoice/aip-speech/upload/voice/'.$mp3.'" controls>您的浏览器不支持 audio 标签。</audio>
